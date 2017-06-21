de7bc3b2e28fe631c2e721b0cbcd8616ff4c5ae8
Vahan13 <vahansargsyan1310@gmail.com>
2017-06-21 13:40:58 +0400

    delete console logs

--- router.js
+++ router.js
@@ -21,12 +21,10 @@ const todos = [
 
 
 router.get('/', (req, res) => {
-    // console.log(req.body);
     res.sendfile('./public/index.html')
 });
 
 router.post('/api/todos', (req, res) => {
-    // console.log(req.body)
     todos.push(req.body);
     res.json(todos);
 })
@@ -36,16 +34,13 @@ router.get('/api/todos', (req, res) => {
 router.delete('/api/todos/:id', (req, res) => {
     const {id} = req.params;
     todos.splice(id, 1);
-    console.log(todos)
     res.json(todos);
 })
 
 router.put('/api/todos/:id', (req, res) => {
     const {id} = req.params;
     const {task} = req.body;
-    console.log('ujhfdsasijfuiysdfc7hsduyfg')
     todos[id] = {task};
-    console.log(todos)
     res.json(todos);
 })
 
