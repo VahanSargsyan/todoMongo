ee4f8c1fe082735aa4ddbd0e75eea5d6cb5a5341
Vahan13 <vahansargsyan1310@gmail.com>
2017-06-21 13:13:28 +0400

    edit and delete function added

--- router.js
+++ router.js
@@ -3,23 +3,51 @@ const valid = require('validator');
 const router = express.Router();
 const data = require('./data');
 
-const todos = {
-    1: 'aaaaaaaaaaaaaa'
-}
+const todos = [
+            {
+                task: 'do something-1'
+                
+            },
+            {
+                task: 'do something-2'
+                
+            },
+            {
+                task: 'do something-3'
+                
+            }
+
+]
 
 
 router.get('/', (req, res) => {
     // console.log(req.body);
     res.sendfile('./public/index.html')
+});
+
+router.post('/api/todos', (req, res) => {
+    // console.log(req.body)
+    todos.push(req.body);
+    res.json(todos);
+})
+router.get('/api/todos', (req, res) => {
+    res.json(todos);
+})
+router.delete('/api/todos/:id', (req, res) => {
+    const {id} = req.params;
+    todos.splice(id, 1);
+    console.log(todos)
+    res.json(todos);
 })
 
-router.post('/add' , (req, res) => {
-    console.log('/addddd');
-    res.redirect('/');
-} )
-router.post('/test', (req, res) => {
-    console.log(req.body);
-    res.redirect('/')
+router.put('/api/todos/:id', (req, res) => {
+    const {id} = req.params;
+    const {task} = req.body;
+    console.log('ujhfdsasijfuiysdfc7hsduyfg')
+    todos[id] = {task};
+    console.log(todos)
+    res.json(todos);
 })
+
 
 module.exports = router;
