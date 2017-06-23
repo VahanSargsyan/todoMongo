483ba2237dcf75730660e362722e4f99c28cd61d
Vahan13 <vahansargsyan1310@gmail.com>
2017-06-23 14:28:53 +0400

    add mongoDB and cookies

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const router = require('./router');
+const uuid = require('uuid/v1');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const PORT = 8080;
 const app = express();
 
-
-
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+   
+    if(!req.cookies.id) {
+        res.cookie('id', uuid())
+    } else {
+        console.log('not need to cookie', req.cookies)
+    }
+    
+    next()
+   
+    
+    
+});
+
+
 app.use('/', router);
-app.use('/add', router)
+app.use('/', (req, res) => {
+    console.log('my cookie is - ',req.cookies);
+})
 
 app.listen(PORT, () => {
     console.log(`run on ${PORT}`)
--- router.js
+++ router.js
@@ -2,46 +2,65 @@ const express = require('express');
 const valid = require('validator');
 const router = express.Router();
 const data = require('./data');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+router.use(cookieParser());
+mongoose.connect('mongodb://localhost:27017/todos');
 
-const todos = [
-            {
-                task: 'do something-1'
-                
-            },
-            {
-                task: 'do something-2'
-                
-            },
-            {
-                task: 'do something-3'
-                
-            }
+const Schema = mongoose.Schema;
+const TaskScheme = new Schema({
+    task: String, 
+    cookie: String, 
+    //created: Date, 
+    // updated: Date
+});
+mongoose.model('Task', TaskScheme);
+const Task = mongoose.model('Task');
 
-]
+const getDB = (req, collection, id) => {
+    console.log('get deb id is', id)
+    collection.find({cookie: 'fakeId'})
+        .then(result => {
+            // console.log('respons is - '+result);
+            req.json(result);
+        })
+        .catch(err =>  console.log(err))
+}    
+
+const insertDB = (el, collection ) =>  collection.create(el);
+const updateDB = (el, id, collection) =>  collection.update({_id: id}, {$set: {task : el}})
+const deleteDB = (id, collection) =>  collection.remove({_id : id})
 
 
 router.get('/', (req, res) => {
-    res.sendfile('./public/index.html')
+    res.sendFile(__dirname + '/public/index.html')
+   
 });
 
 router.post('/api/todos', (req, res) => {
-    todos.push(req.body);
-    res.json(todos);
+    console.log('before', req.cookies)
+    
+    req.body.cookie = `fakeId`;
+    insertDB(req.body, Task)
+        .then(() => getDB(res, Task, req.cookies.id))
+        .catch(err => console.log(err))
 })
 router.get('/api/todos', (req, res) => {
-    res.json(todos);
+    getDB(res, Task, req.cookies);
 })
 router.delete('/api/todos/:id', (req, res) => {
     const {id} = req.params;
-    todos.splice(id, 1);
-    res.json(todos);
+    deleteDB(id, Task)
+        .then(() => getDB(res, Task, req.cookies.id))
 })
 
 router.put('/api/todos/:id', (req, res) => {
     const {id} = req.params;
     const {task} = req.body;
-    todos[id] = {task};
-    res.json(todos);
+    updateDB(task, id, Task)
+        .then(() => getDB(res, Task, req.cookies));
+
+    
 })
 
 
